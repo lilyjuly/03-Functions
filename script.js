86d6b6c13114e7bab3710e12c2f93db88a777a98
lily <lilyjulymm2009@gmail.com>
2024-04-29 20:52:16 +0630

    Function finish

---
+++ script.js
@@ -0,0 +1,23 @@
+function checkEqual(v1,v2){
+    if (v1 === v2){
+        console.log("They are equal in types an values");
+    }else if (v1 == v2){
+        console.log("They are equal in types an values");
+    }else {
+        console.log("They are not equal");
+    }
+};
+checkEqual(100,'100');
+
+
+
+function checkEqual (m1, m2){
+    if (m1 === m2){
+        console.log("They are equal in types and values");
+    }else if (m1 == m2){
+        console.log("They are equal in values")
+    }else{
+        console.log("They are not equal");
+    }
+};
+checkEqual(10,"ten");
